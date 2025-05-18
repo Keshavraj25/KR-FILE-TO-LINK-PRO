81d4600940c16897a056c017688672c8d02cef48
Keshavraj25 <rs9504858@gmail.com>
2025-05-18 17:29:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 
 # fsub  information
 AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')              
-AUTH_CHANNEL = (environ.get("AUTH_CHANNEL", "-100"))
+AUTH_CHANNEL = (environ.get("AUTH_CHANNEL", "-1002409869914"))
 FSUB = environ.get("FSUB", True)
 
 # port information
